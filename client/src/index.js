3948975e15ae98e66fb9d71eae8ec35e1f794ff9
nickless <omar.rguez26@gmail.com>
2024-05-14 09:40:20 -0400

    fix deployment

--- client/src/index.js
+++ client/src/index.js
@@ -34,42 +34,42 @@ import SignUp from "views/index-sections/SignUp";
 import ManageService from "../src/components/Pages/ManageService";
 import ManageProduct from "../src/components/Pages/ManageProduct";
 import RequestQuote from "../src/components/Pages/RequestQuote";
-import Switch from "react-bootstrap-switch";
+// import Switch from "react-bootstrap-switch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  // <BrowserRouter>
-  //   <Routes>
-  //     <Route path="/index" element={<Index />} />
-  //     <Route path="/nucleo-icons" element={<NucleoIcons />} />
-  //     <Route path="/landing-page" element={<LandingPage />} />
-  //     <Route path="/profile-page" element={<ProfilePage />} />
-  //     <Route path="/manage-service" element={<ManageService />} />
-  //     <Route path="/manage-product" element={<ManageProduct />} />
-  //     <Route path="/login-page" element={<LoginPage />} />
-  //     <Route path="/signup-page" element={<SignUp />} />
-  //     <Route path='/request-quote' element={<RequestQuote
+  <BrowserRouter>
+    <Routes>
+      <Route path="/index" element={<Index />} />
+      <Route path="/nucleo-icons" element={<NucleoIcons />} />
+      <Route path="/landing-page" element={<LandingPage />} />
+      <Route path="/profile-page" element={<ProfilePage />} />
+      <Route path="/manage-service" element={<ManageService />} />
+      <Route path="/manage-product" element={<ManageProduct />} />
+      <Route path="/login-page" element={<LoginPage />} />
+      <Route path="/signup-page" element={<SignUp />} />
+      <Route path='/request-quote' element={<RequestQuote
             
-  //     />} />
-  //     <Route path="/" element={<Navigate to="/index" />} />
+      />} />
+      <Route path="/" element={<Navigate to="/index" />} />
 
 
-  //     <Route path="*" element={<Navigate to="/index" replace />} />
-  //   </Routes>
-  // </BrowserRouter>
-  <Router>
-    <Switch>
-      <Route path="/index" component={Index} />
-      {/* <Route path="/nucleo-icons" component={NucleoIcons} />
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} /> */}
-      <Route path="/manage-service" component={ManageService} />
-      <Route path="/manage-product" component={ManageProduct} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/signup-page" component={SignUp} />
-      <Route path='/request-quote' component={RequestQuote} />
-      <Route path="/" component={Index} />
-    </Switch>
-  </Router>
+      <Route path="*" element={<Navigate to="/index" replace />} />
+    </Routes>
+  </BrowserRouter>
+  // <Router>
+  //   <Switch>
+  //     <Route path="/index" component={Index} />
+  //     <Route path="/nucleo-icons" component={NucleoIcons} />
+  //     <Route path="/landing-page" component={LandingPage} />
+  //     <Route path="/profile-page" component={ProfilePage} />
+  //     <Route path="/manage-service" component={ManageService} />
+  //     <Route path="/manage-product" component={ManageProduct} />
+  //     <Route path="/login-page" component={LoginPage} />
+  //     <Route path="/signup-page" component={SignUp} />
+  //     <Route path='/request-quote' component={RequestQuote} />
+  //     <Route path="/" component={Index} />
+  //   </Switch>
+  // </Router>
 );
