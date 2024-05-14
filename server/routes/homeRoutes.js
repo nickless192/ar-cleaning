60aacb8ac88227a012065c8286788ed12de7f60e
nickless <omar.rguez26@gmail.com>
2024-05-13 23:14:45 -0400

    fix deployment

--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.get('/index', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 
-  router.get('/request-quote', (req, res) => {
-    res.render('request-quote');
-    // res.sendFile(path.join(__dirname, '../client/src/components/Pages', 'index.html'));
-  });
+  // router.get('/request-quote', (req, res) => {
+  //   res.render('request-quote');
+  //   // res.sendFile(path.join(__dirname, '../client/src/components/Pages', 'index.html'));
+  // });
 
 module.exports = router;
