bf842d6195740e1038efb59dc0fd890c49d9a34d
nickless <omar.rguez26@gmail.com>
2024-10-27 21:02:10 -0400

    start set up to display reviews in our site

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const apiRoute = require('./api');
 const homeRoutes = require('./homeRoutes');
 
-router.use('/', homeRoutes);
 
 router.use('/api', apiRoute);
+router.use('/', homeRoutes);
 
 
 // router.use((req, res) => {
