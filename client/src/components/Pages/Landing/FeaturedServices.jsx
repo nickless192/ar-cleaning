3111f0072ed2bf700845a8d6297c0cace1562142
nickless <omar.rguez26@gmail.com>
2025-09-01 22:57:52 -0400

    improve landing and add language

--- client/src/components/Pages/Landing/FeaturedServices.jsx
+++ client/src/components/Pages/Landing/FeaturedServices.jsx
@@ -3,27 +3,28 @@ import React, {useState} from "react";
 import { Row, Col, Card, CardBody, CardImg } from "reactstrap";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import { t } from "i18next";
+import { useTranslation } from "react-i18next";
 
-const [services, setServices] = useState([
-  {
-    title: t("products_and_services.carpet_cleaning_title"),
-    img: "/src/assets/img/stock-photo-high-angle-view-person-cleaning-white-carpet-professional-vacuum-cleaner.jpg",
-    link: "/products-and-services#carpet",
-  },
-  {
-    title: t("products_and_services.residential_cleaning_title"),
-    img: "/src/assets/img/man-servant-cleaning-house.jpg",
-    link: "/products-and-services#moveout",
-  },
-  {
-    title: t("products_and_services.commercial_cleaning_title"),
-    img: "/src/assets/img/full-shot-people-cleaning-office.jpg",
-    link: "/products-and-services#commercial",
-  }
-]);
 
 function FeaturedServices() {
+  const { t } = useTranslation();
+  const services = [
+    {
+      title: t("products_and_services.carpet_cleaning_title"),
+      img: "/src/assets/img/stock-photo-high-angle-view-person-cleaning-white-carpet-professional-vacuum-cleaner.jpg",
+      link: "/products-and-services#carpet",
+    },
+    {
+      title: t("products_and_services.residential_cleaning_title"),
+      img: "/src/assets/img/man-servant-cleaning-house.jpg",
+      link: "/products-and-services#moveout",
+    },
+    {
+      title: t("products_and_services.commercial_cleaning_title"),
+      img: "/src/assets/img/full-shot-people-cleaning-office.jpg",
+      link: "/products-and-services#commercial",
+    }
+  ];
   return (
     <div className="py-3 px-2">
       <h2 className="text-center mb-4">{t("featuredServices.heading")}</h2>
