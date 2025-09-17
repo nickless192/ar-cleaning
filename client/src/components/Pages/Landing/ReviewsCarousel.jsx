f20831e22f349d8ff7aaeff60c0407b095583e80
nickless <omar.rguez26@gmail.com>
2025-09-16 23:52:05 -0400

    fix navbar and modals

--- client/src/components/Pages/Landing/ReviewsCarousel.jsx
+++ client/src/components/Pages/Landing/ReviewsCarousel.jsx
@@ -12,25 +12,42 @@ function ReviewsCarousel() {
 
   const reviews = [
     {
+      text: "Highly recommend the team at CleanARSolutions! My house was sparkling from top to bottom! They even got some stains out that my previous cleaner couldn’t!",
+      author: "Shimki C.",
+      stars: 5,
+      source: "Google Reviews", 
+    },
+    {
+      text: "Great experience! Our home was left spotless and fresh. Very reliable, thorough, and professional. Highly recommend!",
+      author: "Ali S.",
+      stars: 5,
+      source: "Google Reviews",
+    },
+    {
       text: "Just moved to my new condo and used CleanAR Salution for a deep clean - absolutely fantastic service -Fili & Omar-! Their team was professional, thorough, and made my new place spotless. The attention to detail was impressive, and they left everything pristine for my move-in. Highly recommend CleanAR for anyone needing quality cleaning services!",
       author: "Angel G.",
       stars: 5,
+      source: "Google Reviews",
     },
     {
       text: `So happy to have found CleanAR solutions. They were highly recommended by my sister. Filiberto and Marian cleans our home so well! They are kind, thorough, efficient and so professional! We really appreciate the reasonable price, availability, and their variety of cleaning options. Thank you CleanAR for servicing our home!`,
       author: "Seya D.",
       stars: 5,
+      source: "Google Reviews",
     },
     {
       text: "We recently hired Fili from Cleanar Solutions to clean our windows, and they did an absolutely fantastic job! From start to finish, their service was professional, efficient, and incredibly thorough. Our windows have never looked this spotless and bright. Fili’s attention to detail was evident, and he ensured every window sparkled by the time they were done. Not only was the quality of their work impressive, but they were on time, friendly and provided excellent service at a reasonable price. We couldn't be happier with the results and would highly recommend Cleanar Solutions to anyone looking for top-notch cleaning. Thank you, Cleanar Solutions!",
       author: "Nikita C.",
       stars: 5,
+      source: "Google Reviews",
     },
     {
       text: "Booking CleanAR Solutions was fast and easy to do! They were super professional and left the place looking shiny and new! They did a deep clean of our bathroom, and steam cleaned our carpet. The tile has never sparkled so bright, they put everything back in its place, and we had an overall great experience. We will definitely be using their services again for our next job.",
       author: "Alex C.",
       stars: 5,
+      source: "Google Reviews",
     },
+    
   ];
 
   const next = () => setIndex((i) => (i + 1) % reviews.length);
